9b6f63a0a4e5c9a337c1c3d6f04aac27865f8400
leonardobarral <leonardobarral2@gmail.com>
2024-05-27 20:10:37 -0300

    Minhas modificações

---
+++ src/main/java/br/com/fiap/nutrivibe/nutrivibe/config/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package br.com.fiap.nutrivibe.nutrivibe.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private VerificarToken verificarToken;
+
+
+
+}
